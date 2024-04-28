6de94ca2a92ecbba96c0e139686e9207584e9328
Abdullah-Jammal <129687747+Abdullah-Jammal@users.noreply.github.com>
2024-04-28 11:24:46 +0300

    newUpdate

--- components/Video.jsx
+++ components/Video.jsx
@@ -5,7 +5,7 @@ export default function Video({ handelShowVideo }) {
     <div className="watchVideo">
       <span className="closeNewVideo" onClick={() => handelShowVideo()}>X</span>
       <h2>برعاية معالي وزير التربية الدكتور ابراهيم نامس الجبوري وبأشراف مباشر من مديرية التلفزيون التربوي . تربية نينوى تعلن جاهزيتها لاستلام طلبات المتقدمين للمدرسة</h2>
-      <video width="320" height="240" controls preload="none">
+      <video width="520" height="440" controls preload="none">
         <source src="/Videos/newVideo.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
